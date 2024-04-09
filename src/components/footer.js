b583ff2648444b8519a4b24f58f248d3eac31482
Preston Tom-Ying <preston.tomying@gmail.com>
2024-04-08 23:59:21 -0400

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,6 @@
 import { SocialIcon } from "react-social-icons";
 
-// nav buttons use <div> and <a> instead of <button>
 export default function Footer() {
-    // Optional tailwind (add this when u call this element: bg-cloudBurst, text-[white], )
     return (
         <div className="flex items-center justify-center pt-16 pb-10">
             <p className="mr-4">Follow Me on Socials</p>

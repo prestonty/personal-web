4678d42cef4640a1a923b910faf41841d4dab724
Preston <preston.tomying@gmail.com>
2026-04-13 02:04:28 -0400

    update resume

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -57,7 +57,7 @@ export default function HeroSection() {
                             />
                             <a
                                 className="font-black text-2xl ml-3 hover:text-blue duration-300"
-                                href="https://drive.google.com/file/d/1eCCiZEa7RhhyalzLQQ-7LC3VacU8i6Ts/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1Q9iqbVfGTwA20MRUSnJab-eNZ2niLJWV/view?usp=sharing"
                                 target="_blank"
                                 rel="noreferrer"
                             >
